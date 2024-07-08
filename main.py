3e30cfd0e6b7aaabd29ba3215030eb5234a5abd5
RaimundoRobotica <raimundo.robotica@gmail.com>
2024-07-08 18:45:58 +0200

    ORDERING MAIN

--- main.py
+++ main.py
@@ -1,55 +1,10 @@
-import fitz
-import os
-from files import *
+from read import buscar_palabras_en_pdf
+from files import crear_carpeta
 
-emisores_factura = ['vvalley']
-clientes = ['afanias', 'mopsa', 'aora', 'macrison', 'isq', 'sanfiz', 'legisfund', 'audiconsulting', 'cualicontrol', '40db','moreno', 'saniscle', 'zitro']
-conceptos = ['microsoft365']
+ruta_carpeta = './facturas'
+proveedores = ['V-Valley', 'Repsol', 'INGRAM', 'Infratech']
 
-def renombrar_archivos_pdf(carpeta):
-    archivos = os.listdir(carpeta)
-    contador = 1
-    for archivo in archivos:
-        ruta_completa = os.path.join(carpeta, archivo)
-        if os.path.isfile(ruta_completa) and archivo.lower().endswith('.pdf'):
-            # Crear el nuevo nombre del archivo
-            nuevo_nombre = f"hola_{contador}.pdf"
-            nueva_ruta = os.path.join(carpeta, nuevo_nombre)
-            # Renombrar el archivo
-            os.rename(ruta_completa, nueva_ruta)
-            contador += 1
-    print(f"Renombrados {contador-1} archivos PDF.")
-
-def listar_archivos_en_carpeta(carpeta):
-    archivos = os.listdir(carpeta)
-    for archivo in archivos:
-        # Crear la ruta completa del archivo
-        ruta_completa = os.path.join(carpeta, archivo)
-        if os.path.isfile(ruta_completa):
-            print(f"Archivo: {archivo}")
-
-listar_archivos_en_carpeta('./')
-
-
-def leer_pdf(ruta_archivo):
-    # Abrir el archivo PDF
-    documento = fitz.open(ruta_archivo)
-    
-    # Inicializar una variable para almacenar el texto
-    texto_completo = ""
-    
-    # Iterar sobre cada página del PDF
-    for num_pagina in range(documento.page_count):
-        pagina = documento.load_page(num_pagina)  # Cargar la página
-        texto = pagina.get_text()  # Extraer el texto de la página
-        print(texto)  # Agregar el texto de la página al texto completo
-    
-    # Cerrar el documento PDF
-    documento.close()
-    
-    return texto_completo
-
-# Ejemplo de uso
-ruta_archivo = './prueba.PDF'
-texto = leer_pdf(ruta_archivo)
-print(texto)
+if __name__ == '__main__':
+    crear_carpeta(ruta_carpeta + '/Pend_Archivar')
+    crear_carpeta(ruta_carpeta + '/sin_clasificar')
+    buscar_palabras_en_pdf(ruta_carpeta, proveedores)
--- read.py
+++ read.py
@@ -5,17 +5,17 @@ from vvalley import extract_vvalley_data, crear_nombre_factura_vvalley
 from ingram import extract_ingram_data, crear_nombre_factura_ingram
 #from infratech import extract_infratech_data, crear_nombre_factura_infratech
 
-from files import crear_carpeta, copiar_y_renombrar_pdf
-
-proveedores = ['V-Valley', 'TeamViewer', 'Repsol', 'INGRAM', 'Infratech']
-ruta_carpeta = './facturas'
+from files import copiar_y_renombrar_pdf
 
 def buscar_palabras_en_pdf(ruta_carpeta, proveedores):
     procesados = []
+    todos_los_archivos = []
     # Recorrer todos los archivos en la carpeta
-    for nombre_archivo in os.listdir(ruta_carpeta):
+    lista_rutas = os.listdir(ruta_carpeta)
+    for nombre_archivo in lista_rutas:
         # Verificar que el archivo sea un PDF
         if nombre_archivo.endswith('.pdf') or nombre_archivo.endswith('.PDF'):
+            todos_los_archivos.append(nombre_archivo)
             ruta_archivo = os.path.join(ruta_carpeta, nombre_archivo)
             # Leer el contenido del PDF
             with open(ruta_archivo, 'rb') as archivo_pdf:
@@ -29,7 +29,7 @@ def buscar_palabras_en_pdf(ruta_carpeta, proveedores):
                 for palabra in proveedores:
                     if palabra in texto_completo:
                         print(f'La palabra "{palabra}" fue encontrada en el archivo "{nombre_archivo}".')
-                        procesados.append(archivo_pdf)
+                        procesados.append(nombre_archivo)
                         if palabra == 'V-Valley':
                             data = extract_vvalley_data(texto_completo)
                             nombre_nuevo_archivo = crear_nombre_factura_vvalley(data)
@@ -40,15 +40,9 @@ def buscar_palabras_en_pdf(ruta_carpeta, proveedores):
                             copiar_y_renombrar_pdf(ruta_carpeta, nombre_archivo, ruta_carpeta + '/Pend_Archivar', nombre_nuevo_archivo)
                         if palabra == 'Repsol':
                             copiar_y_renombrar_pdf(ruta_carpeta, nombre_archivo, ruta_carpeta + '/Pend_Archivar', nombre_archivo)
-                   
-    for nombre_archivo in os.listdir(ruta_carpeta):
-                # Verificar que el archivo sea un PDF
+    print('Copiando archivos no reconocidos')
+    for nombre_archivo in todos_los_archivos:
         if nombre_archivo not in procesados:
-            copiar_y_renombrar_pdf(ruta_archivo, nombre_archivo, ruta_archivo + '/sin_clasificar', nombre_archivo)
+            copiar_y_renombrar_pdf(ruta_carpeta, nombre_archivo, ruta_carpeta + '/sin_clasificar', nombre_archivo)
                         
 
-
-crear_carpeta(ruta_carpeta + '/Pend_Archivar')
-crear_carpeta(ruta_carpeta + '/sin_clasificar')
-
-buscar_palabras_en_pdf(ruta_carpeta, proveedores)
